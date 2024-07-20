ab5fbd78b637c8ff2fae72053ef78542ef00c572
Parshad <138670180+parshadk@users.noreply.github.com>
2024-07-21 01:00:59 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,18 @@
-const withMT = require("@material-tailwind/react/utils/withMT");
 /** @type {import('tailwindcss').Config} */
-module.exports =  withMT({
+module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
-      screens: {
-        'sx': { "max": '550px'},
-        // => @media (min-width: 640px) { ... }
-      },
-
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      }
+    }
   },
-  plugins: [],
-});
+  plugins: [require('tailwind-scrollbar-hide'), require('tailwindcss-animated')]
+};
